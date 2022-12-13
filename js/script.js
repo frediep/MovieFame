ed68a6b453fabba07c0af620504153ea2ff535fe
frediep <117768642+frediep@users.noreply.github.com>
2022-12-13 17:43:33 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function ver_s(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'LA SERIE ESTÁ LISTA PARA VER',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
+
+function ver_p(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'LA PELICULA ESTÁ LISTA PARA VER',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
+
+function ver_an(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'EL ANIME ESTÁ LISTA PARA VER',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
